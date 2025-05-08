e5af89ac36ffb625a4ed1eaa4791210781c9d103
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-08 16:20:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+Console.WriteLine(MissingNumber(nums));
+
+
+int MissingNumber(int[] nums)
+{
+    int n = nums.Length;
+    int result = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        result ^= i;
+    }
+    foreach (int num in nums)
+    {
+        result ^= num;
+    }
+    return result;
+}
